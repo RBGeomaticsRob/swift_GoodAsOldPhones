15eb1a1aeec1e30531ed7cb2c41a115e5e51df23
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2016-02-02 20:23:03 +0000

    Add data to table items

---
+++ GoodAsOldPhones/ProductsTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ProductsTableViewController.swift
+//  GoodAsOldPhones
+//
+//  Created by Robert Bowers on 02/02/2016.
+//  Copyright © 2016 Robert Bowers. All rights reserved.
+//
+
+import UIKit
+
+class ProductsTableViewController: UITableViewController {
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ProductCell", forIndexPath: indexPath)
+        cell.textLabel?.text = "Hello Friend"
+        cell.imageView?.image = UIImage(named: "image-cell1")
+        return cell
+    }
+
+
+}
