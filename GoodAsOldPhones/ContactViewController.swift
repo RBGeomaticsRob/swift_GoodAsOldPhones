1eb0fba4dce4ad2416371d36eef733b120b656c0
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2016-02-02 19:25:35 +0000

    Implemented a Scroll View

--- GoodAsOldPhones/ContactViewController.swift
+++ GoodAsOldPhones/ContactViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 
 class ContactViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.addSubview(scrollView)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        scrollView.contentSize = CGSizeMake(375,800)
     }
 
 }
