12de24b431fe4b78aad6884a02426026a533b754
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2016-02-03 19:38:56 +0000

    Created a Product class to store all project attributes in an object for easy portability

---
+++ GoodAsOldPhones/Product.swift
@@ -0,0 +1,15 @@
+//
+//  Product.swift
+//  GoodAsOldPhones
+//
+//  Created by Robert Bowers on 03/02/2016.
+//  Copyright © 2016 Robert Bowers. All rights reserved.
+//
+
+import Foundation
+
+class Product {
+    var name: String?
+    var productImage: String?
+    var cellImage: String?
+}
